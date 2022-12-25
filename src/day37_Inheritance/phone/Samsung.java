2074be60247c010366cc99b34632ce34708beaf1
Hüseyin Yıldırım <11huseyinyildirim@gmail.com>
2022-12-25 12:04:15 +0100

    Day 41 Exception intro

--- src/day37_Inheritance/phone/Samsung.java
+++ src/day37_Inheritance/phone/Samsung.java
@@ -16,4 +16,9 @@ public class Samsung extends Phone{
     public Samsung(String model, String size, double price, String color) {
         super("Samsung", model, size, price, color);
     }
+
+
+
+
+
 }
